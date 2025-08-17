a94d01107550f3039e29441e075696d2be6310fe
ingkarat <key.ingkarat.29@gmail.com>
2025-08-17 14:31:07 +0700

    feat(cart, order): add cart and order modules with controllers, services, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -19,6 +22,9 @@ import { CategoriesModule } from './categories/categories.module';
     ProductsModule,
     AuthModule,
     CategoriesModule,
+    CartModule,
+    OrderModule,
+    PaymentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
